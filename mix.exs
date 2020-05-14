01a3768fc5c8ed49efbfc0d53994d0a36432ba7f
c4710n <c4710n@users.noreply.github.com>
2020-05-14 10:34:46 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Scrivener.HTML.MixProject do
   use Mix.Project
 
-  @version "2.0.1"
+  @version "2.1.0"
   @github_url "https://github.com/c4710n/scrivener_html_semi"
 
   def project do
